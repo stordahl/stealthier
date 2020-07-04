b50af6c28189cb48d71fe1e30aeab116cce1fe38
stordahl <jacob@jacobstordahl.com>
2020-07-03 17:07:25 -0700

    Update header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,5 @@
 <header>
-    <h1>stealthier</h1>
+    <h1>stealthier <span id="beta">(beta)</span></h1>
     <h3>a vanlife community resource</h3>
 </header>
 
@@ -13,6 +13,10 @@
     }
     h1{
         font-size:3rem;
+    }
+    #beta{
+        font-size:1.3rem;
+        font-style: initial;
     }
     @media screen and (min-width:800px){
         header{
